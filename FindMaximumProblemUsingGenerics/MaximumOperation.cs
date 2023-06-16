e6e252c323fc0103a775ac206e517c62e1284084
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-16 07:27:16 +0530

    [somu]Add:Finding Maximum Integer.

---
+++ FindMaximumProblemUsingGenerics/MaximumOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumProblemUsingGenerics
+{
+    public class MaximumOperation
+    {
+        public void Maximum(int a, int b, int c)
+        {
+            if(a.CompareTo(b) >0 && a.CompareTo(c) >0)
+                Console.WriteLine("{0} is Maximum",a);
+            if(b.CompareTo(a)>0 && b.CompareTo(c) >0)
+                Console.WriteLine("{0} is Maximum",b);
+            if(c.CompareTo(a)>0 && c.CompareTo(b)>0)
+                Console.WriteLine("{0} is Maximum",c);
+        }
+    }
+}
---
+++ FindMaximumProblemUsingGenerics/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FindMaximumProblemUsingGenerics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag=true;
+            while(flag)
+            {
+                Console.WriteLine("Choose Option to Perform\n1.Find Maximum Integers\n2.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                MaximumOperation operation = new MaximumOperation();
+                switch(option)
+                {
+                    case 1:
+                        operation.Maximum(39,23,35);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
